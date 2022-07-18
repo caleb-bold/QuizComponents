c3a833b42526db6d0d53a9dded81fa192a819a93
caleb-bold <92266540+caleb-bold@users.noreply.github.com>
2022-07-18 23:57:48 +0900

    m

--- src/ImageLoader.js
+++ src/ImageLoader.js
@@ -67,8 +67,7 @@ export default class ImageLoader
         let choices = this.m_imageQueue[0]; //.shift();
         if (choices.correct.isLoaded()==1 && choices.wrong.isLoaded()==1) {
             if (this.m_cases.length > 0) {
-                let case = this.m_cases.shift();
-
+                console.log(this.m_cases.length);
             }
             /*if (this.m_cases.length > 0) {
                 let case = this.m_cases.shift();
