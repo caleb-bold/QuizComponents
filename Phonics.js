f4dbe6625cba89d296678a132948e588bc4ed97a
caleb-bold <92266540+caleb-bold@users.noreply.github.com>
2022-07-24 21:46:55 +0900

    m

--- Phonics.js
+++ Phonics.js
@@ -8,11 +8,11 @@ import Button from "./src/Button.js";
 import Image from "./src/Image.js";
 import Progress from "./src/Progress.js";
 import ImageLoader from "./src/ImageLoader.js";
-import RealtimeScore from "./src/RealtimeScore.js";
+//import RealtimeScore from "./src/RealtimeScore.js";
 
 
-let realtime_score = new RealtimeScore();
-realtime_score.setZero();
+//let realtime_score = new RealtimeScore();
+//realtime_score.setZero();
 
 let progress = new Progress();
 progress.setZero();
@@ -33,7 +33,7 @@ wrong.setSrc("./img/wrong.png");
 wrong.setInvisible();
 
 let quiz = new Quiz();
-quiz.appendChild('realtime_score', realtime_score);
+//quiz.appendChild('realtime_score', realtime_score);
 quiz.appendChild('progress', progress);
 quiz.appendChild('question', question);
 
@@ -216,7 +216,7 @@ function chooseAnswer(e) {
 
     if((e.keyCode == 49 && answerIdx == 0) || (e.keyCode == 48 && answerIdx == 1)) {
         correct.setVisible();
-        realtime_score.addScore(100);
+        //realtime_score.addScore(100);
     }
     else {
         wrong.setVisible();
@@ -253,12 +253,12 @@ function clickStart() {
     //searchBLE();
 
     progress.setProgress(0, total);
-    realtime_score.addScore(0);
+    //realtime_score.addScore(0);
     root.deleteChild('start');
     page.appendChild('quiz', quiz);
     question.adjustFontSize();
     progress.adjustFontSize();
-    realtime_score.adjustFontSize();
+    //realtime_score.adjustFontSize();
     
     window.removeEventListener("keyup", clickStart);
 
