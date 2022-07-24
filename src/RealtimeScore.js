002dcbc35279b0026593bd4c2f0d6f6f67add464
caleb-bold <92266540+caleb-bold@users.noreply.github.com>
2022-07-24 21:28:54 +0900

    m

--- src/RealtimeScore.js
+++ src/RealtimeScore.js
@@ -14,14 +14,14 @@ export default class RealtimeScore extends Component {
         this.m_objBox = Component.createBox(20, 10, 30, -30, true);
         this.m_objBox.table.style.backgroundColor = '#777777';
         this.m_objCurrentDOM = this.m_objBox.table;
-        this.m_objBox.td.innerHTML = '0';
+        this.m_objBox.td.innerHTML = "0";
     }
 
     /**
      * A reset function.
      */
      reset() {
-        this.m_objBox.td.innerHTML = '0';
+        this.m_objBox.td.innerHTML = "0";
         super.reset();
     }
 
@@ -30,7 +30,7 @@ export default class RealtimeScore extends Component {
 
         this.m_objBox.td.style.fontSize = Component.getObjectHeight(0) + 'px';
         this.m_objBox.td.style.fontWeight = '500';
-        this.m_objBox.td.innerHTML = ' ' + this.m_nScore + ' ';
+        this.m_objBox.td.innerHTML = "[" + this.m_nScore + "]";
 
         setTimeout(() => {
             Component.adjustFontSize(this.m_objBox.table, this.m_objBox.td, 10);
@@ -45,6 +45,6 @@ export default class RealtimeScore extends Component {
 
     addScore(score) {
         this.m_nScore += score;
-        this.m_objBox.td.innerHTML = ' ' + this.m_nScore + ' ';
+        this.m_objBox.td.innerHTML = "[" + this.m_nScore + "]";
     }
 }
