c7be445ee831a270b58ae4f7b944830b41c5b757
caleb-bold <92266540+caleb-bold@users.noreply.github.com>
2022-07-30 14:04:32 +0900

    m

--- Phonics.js
+++ Phonics.js
@@ -275,7 +275,7 @@ function chooseAnswer(e) {
 
     if((e.keyCode == 49 && answerIdx == 0) || (e.keyCode == 48 && answerIdx == 1)) {
         
-        correct_sound.stop();
+        //correct_sound.stop();
         //correct_sound.pause();
         //correct_sound.currentTime = 0;
         correct_sound.volume = 1.0;
