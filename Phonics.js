7991f95a208ee575d87a181418d0bcb79592eff4
caleb-bold <92266540+caleb-bold@users.noreply.github.com>
2022-07-30 14:06:29 +0900

    m

--- Phonics.js
+++ Phonics.js
@@ -17,9 +17,6 @@ let music = new Audio('./sound/mixkit-game-level-music-689.wav');
 music.loop = true;
 music.volume = 1.0;
 
-let correct_sound = new Audio('./sound/mixkit-winning-a-coin-video-game-2069.wav');
-correct_sound.loop = false;
-correct_sound.volume = 1.0;
 
 
 let final_score = new FinalScore();
@@ -273,12 +270,16 @@ function chooseAnswer(e) {
     stateKeyUp = false;
     window.addEventListener("keyup", setKeyUp);
 
-    if((e.keyCode == 49 && answerIdx == 0) || (e.keyCode == 48 && answerIdx == 1)) {
-        
+    if((e.keyCode == 49 && answerIdx == 0) || (e.keyCode == 48 && answerIdx == 1))
+    {
+        let correct_sound = new Audio('./sound/mixkit-winning-a-coin-video-game-2069.wav');
+        correct_sound.loop = false;
+        correct_sound.volume = 1.0;
+
         //correct_sound.stop();
         //correct_sound.pause();
         //correct_sound.currentTime = 0;
-        correct_sound.volume = 1.0;
+        //correct_sound.volume = 1.0;
         correct_sound.play();
         
         correct.setVisible();
